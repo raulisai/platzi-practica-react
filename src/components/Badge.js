78df77e40b402288e569d730a310f0142048e3c3
IsaiMonroy <raulisai97@gmail.com>
2023-01-10 00:10:05 -0600

    first commit

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,3 +1,4 @@
+//import { NEWOBJECT.JS } from './NEWOBJECT.JS'
 import React from 'react';
 
 import './styles/Badge.css';
@@ -7,28 +8,28 @@ class Badge extends React.Component {
   render() {
     return (
       <div className="Badge">
-        <div className="Badge__header">
-          <img src={confLogo} alt="Logo de la conferencia" />
-        </div>
-
-        <div className="Badge__section-name">
-          <img
-            className="Badge__avatar"
-            src={this.props.avatarUrl}
-            alt="Avatar"
-          />
-          <h1>
-            {this.props.firstName} <br /> {this.props.lastName}
-          </h1>
-        </div>
-
-        <div className="Badge__section-info">
-          <h3>{this.props.jobTitle}</h3>
-          <div>@{this.props.twitter}</div>
-        </div>
-
-        <div className="Badge__footer">#platziconf</div>
+      <div className="Badge__header">
+        <img src={confLogo} alt="Logo de la conferencia" />
       </div>
+
+      <div className="Badge__section-name">
+        <img
+          className="Badge__avatar"
+          src={this.props.avatarUrl}
+          alt="Avatar"
+        />
+        <h1>
+          {this.props.firstName} <br /> {this.props.lastName}
+        </h1>
+      </div>
+
+      <div className="Badge__section-info">
+        <h3>{this.props.jobTitle}</h3>
+        <div>@{this.props.twitter}</div>
+      </div>
+
+      <div className="Badge__footer">#platziconf</div>
+    </div>
     );
   }
 }
